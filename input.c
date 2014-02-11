efd60bedf11c4ef21d1c3c0987de1025be03b2a3
bobtwinkles <srkoser+GitHub@gmail.com>
2014-02-10 20:02:54 -0500

    allow quiting with escape

--- input.c
+++ input.c
@@ -11,6 +11,11 @@ void handle_events(void) {
   while (SDL_PollEvent(&event)) {
     switch(event.type) {
       case SDL_QUIT: exit(0); break;
+      case SDL_KEYDOWN:
+        if (event.key.keysym.sym == SDLK_ESCAPE) {
+          exit(0);
+        }
+        break;
     }
   }
 }
